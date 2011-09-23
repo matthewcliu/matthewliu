c0cceb4b00c8fa3b49c5a80a7c6e05fa63a4a5b1
Matthew Liu <matthewliu@gmail.com>
2011-09-22 22:46:43 -0700

    Added FB Timeline

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Matthewliu::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +55,9 @@ Matthewliu::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  
+  root :to => 'pages#home'
+  
 
   # See how all your routes lay out with "rake routes"
 
